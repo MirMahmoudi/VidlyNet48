f0e291d300a905bf257aeda68375239b0ed96f5d
MirMahmoudi <mirhabib.mahmoudi@gmail.com>
2023-04-24 16:02:18 +0200

    Creating Movies and Customers

--- VidlyNet48.Presentation/Controllers/HomeController.cs
+++ VidlyNet48.Presentation/Controllers/HomeController.cs
@@ -13,6 +13,7 @@ namespace VidlyNet48.Presentation.Controllers
             return View();
         }
 
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
---
+++ VidlyNet48.Presentation/Controllers/MoviesController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using VidlyNet48.Presentation.Models;
+using VidlyNet48.Presentation.ViewModels;
+
+namespace VidlyNet48.Presentation.Controllers
+{
+	public class MoviesController : Controller
+	{
+		// GET: Movies
+		public ActionResult Index()
+		{
+			var movies = GetMovies();
+
+			return View(movies);
+		}
+
+		// GET: Movies/Random
+		public ActionResult Random()
+		{
+			var movie = new Movie { Name = "Shrek" };
+			var customers = new List<Customer>
+			{
+				new Customer{Name = "Customer1"},
+				new Customer{Name = "Customer2"}
+			};
+
+			var viewModel = new RandomMovieViewModel
+			{
+				Movie = movie,
+				Customers = customers
+			};
+
+			return View(viewModel);
+		}
+
+		private IEnumerable<Movie> GetMovies()
+		{
+			return new List<Movie>
+			{
+				new Movie {Name = "Shrek"},
+				new Movie {Name = "Wall-e"}
+			};
+		}
+	}
+}
