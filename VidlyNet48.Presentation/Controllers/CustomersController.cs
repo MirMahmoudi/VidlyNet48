9c5641b6830a3c5ca136d47b1805695f12ae7d5f
MirMahmoudi <mirhabib.mahmoudi@gmail.com>
2023-07-05 14:21:59 +0200

    editing controller based on db initialization

--- VidlyNet48.Presentation/Controllers/CustomersController.cs
+++ VidlyNet48.Presentation/Controllers/CustomersController.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
 using VidlyNet48.Presentation.Models;
@@ -7,10 +7,23 @@ namespace VidlyNet48.Presentation.Controllers
 {
 	public class CustomersController : Controller
 	{
+		private readonly ApplicationDbContext _context;
+
+		public CustomersController()
+		{
+			_context = new ApplicationDbContext();
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			_context.Dispose();
+		}
+
 		// GET: Customers
 		public ActionResult Index()
 		{
-			var customers = GetCustomers();
+			var customers = _context.Customers
+				.Include(c => c.MembershipType);
 
 			return View(customers);
 		}
@@ -18,19 +31,12 @@ namespace VidlyNet48.Presentation.Controllers
 		// GET: Customers/Details/Id
 		public ActionResult Details(int id)
 		{
-			var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+			var customer = _context.Customers
+				.Include(c => c.MembershipType)
+				.SingleOrDefault(c => c.Id == id);
 
 			if (customer is null) return HttpNotFound();
 			return View(customer);
-		}
-
-		private IEnumerable<Customer> GetCustomers()
-		{
-			return new List<Customer>
-			{
-				new Customer { Id = 1, Name = "John Smith" },
-				new Customer { Id = 2, Name = "Mary Williams" }
-			};
 		}
 	}
 }
