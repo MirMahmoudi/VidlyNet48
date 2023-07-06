843a7211eca6846e2afb6f3730d016871a18ccef
MirMahmoudi <mirhabib.mahmoudi@gmail.com>
2023-07-06 19:35:45 +0200

    Movies Validation

--- VidlyNet48.Presentation/ViewModels/MovieFormViewModel.cs
+++ VidlyNet48.Presentation/ViewModels/MovieFormViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using VidlyNet48.Presentation.Models;
 
 namespace VidlyNet48.Presentation.ViewModels
@@ -6,16 +8,36 @@ namespace VidlyNet48.Presentation.ViewModels
 	public class MovieFormViewModel
 	{
 		public IEnumerable<Genre> Genres { get; set; }
-		public Movie Movie { get; set; }
+		public int? Id { get; set; }
 
-		public string Title
+		[Required, StringLength(255)]
+		public string Name { get; set; }
+
+		[Required, Display(Name = "Genre")]
+		public int? GenreId { get; set; }
+
+
+		[Required, Display(Name = "Release Date")]
+		public DateTime? ReleaseDate { get; set; }
+
+		[Required, Display(Name = "Number in stock")]
+		[Range(1, 20, ErrorMessage = "Should be between 1 and 20!")]
+		public int? NumberInStuck { get; set; }
+
+		public string Title => Id != 0 ? "Edit Movie" : "New Movie";
+
+		public MovieFormViewModel()
 		{
-			get
-			{
-				if (Movie != null && Movie.Id != 0)
-					return "Edit Movie";
-				return "New Movie";
-			}
+			Id = 0;
+		}
+
+		public MovieFormViewModel(Movie movie)
+		{
+			Id = movie.Id;
+			Name = movie.Name;
+			GenreId = movie.GenreId;
+			NumberInStuck = movie.NumberInStuck;
+			ReleaseDate = movie.ReleaseDate;
 		}
 	}
 }
