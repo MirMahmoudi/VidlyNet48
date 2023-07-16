6b81e4b8f3a57c09e0b8168fed31e8687fdb221a
MirMahmoudi <mirhabib.mahmoudi@gmail.com>
2023-07-16 20:21:01 +0200

    Dto

---
+++ VidlyNet48.Presentation/Dto/CustomerDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace VidlyNet48.Presentation.Dto
+{
+	public class CustomerDto
+	{
+		public int Id { get; set; }
+
+		[Required, StringLength(255)]
+		public string Name { get; set; }
+
+		public bool IsSubscribedToNewsletter { get; set; }
+
+		public int MembershipTypeId { get; set; }
+
+		// [Min18YearsIfMember]
+		public DateTime? Birthdate { get; set; }
+	}
+}
---
+++ VidlyNet48.Presentation/Dto/MovieDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace VidlyNet48.Presentation.Dto
+{
+	public class MovieDto
+	{
+		public int Id { get; set; }
+
+		[Required, StringLength(255)]
+		public string Name { get; set; }
+
+		[Required]
+		public int GenreId { get; set; }
+
+		public DateTime? DateAdded { get; set; }
+
+		public DateTime ReleaseDate { get; set; }
+
+		[Range(1, 20)]
+		public int NumberInStuck { get; set; }
+	}
+}
