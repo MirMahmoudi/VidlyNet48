ede562383ee05e22538ba680308eaf761cce8d80
MirMahmoudi <mirhabib.mahmoudi@gmail.com>
2023-07-16 22:07:43 +0200

    WebApi Config

--- VidlyNet48.Presentation/Global.asax.cs
+++ VidlyNet48.Presentation/Global.asax.cs
@@ -1,10 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using AutoMapper;
+using VidlyNet48.Presentation.App_Start;
 
 namespace VidlyNet48.Presentation
 {
@@ -12,10 +11,12 @@ namespace VidlyNet48.Presentation
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Mapper.Initialize(c => c.AddProfile<MappingProfiles>());
         }
     }
 }
