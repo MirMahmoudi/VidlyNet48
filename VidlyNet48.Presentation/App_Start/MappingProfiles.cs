86d3d01352ee6697208b818949452dfb0ae94304
MirMahmoudi <mirhabib.mahmoudi@gmail.com>
2023-07-18 15:48:05 +0200

    Genre & Membership Mapping

--- VidlyNet48.Presentation/App_Start/MappingProfiles.cs
+++ VidlyNet48.Presentation/App_Start/MappingProfiles.cs
@@ -13,10 +13,16 @@ namespace VidlyNet48.Presentation.App_Start
 			Mapper.CreateMap<CustomerDto, Customer>()
 				.ForMember(c => c.Id, opt => opt.Ignore());
 
+			// MembershipType
+			Mapper.CreateMap<MembershipType, MembershipTypeDto>();
+
 			// Movie
 			Mapper.CreateMap<Movie, MovieDto>();
 			Mapper.CreateMap<MovieDto, Movie>()
 				.ForMember(m => m.Id, opt => opt.Ignore());
+
+			// Genre
+			Mapper.CreateMap<Genre, GenreDto>();
 		}
 	}
 }
