c158cffe9cc7a24eb7ac5557002fbfb10e0f21b9
MirMahmoudi <mirhabib.mahmoudi@gmail.com>
2023-07-18 15:47:35 +0200

    Bundle Config

--- VidlyNet48.Presentation/App_Start/BundleConfig.cs
+++ VidlyNet48.Presentation/App_Start/BundleConfig.cs
@@ -3,28 +3,37 @@ using System.Web.Optimization;
 
 namespace VidlyNet48.Presentation
 {
-    public class BundleConfig
-    {
-        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
-        public static void RegisterBundles(BundleCollection bundles)
-        {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+	public class BundleConfig
+	{
+		// For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+		public static void RegisterBundles(BundleCollection bundles)
+		{
+			bundles.Add(new Bundle("~/bundles/lib").Include(
+				"~/Scripts/jquery-{version}.js",
+				"~/Scripts/bootstrap.js",
+				"~/Scripts/bootbox.js",
+				"~/Scripts/DataTables/jquery.dataTables.js",
+				"~/Scripts/DataTables/dataTables.bootstrap.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+				// bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+			// 			"~/Scripts/jquery-{version}.js"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+						"~/Scripts/jquery.validate*"));
 
-            bundles.Add(new Bundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+			// Use the development version of Modernizr to develop with and learn from. Then, when you're
+			// ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+						"~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap-lumen.css",
-                      "~/Content/Site.css"));
-        }
-    }
+			// bundles.Add(new Bundle("~/bundles/bootstrap").Include(
+			// 		  "~/Scripts/bootstrap.js",
+			// 		  "~/Scripts/bootbox.js"));
+
+			bundles.Add(new StyleBundle("~/Content/css").Include(
+					  "~/Content/bootstrap-lumen.css",
+					  "~/Content/DataTables/css/dataTables.bootstrap4.css",
+					  "~/Content/Site.css"));
+		}
+	}
 }
