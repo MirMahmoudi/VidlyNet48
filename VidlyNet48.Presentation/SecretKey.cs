ddc5b369f097bd322e6d1df866556005a67af7e6
Mir Habib Mahmoudi <66380429+MirMahmoudi@users.noreply.github.com>
2023-07-26 10:49:45 +0200

    delete the key

--- VidlyNet48.Presentation/SecretKey.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace VidlyNet48.Presentation
-{
-	public static class SecretKey
-	{
-		public const string facebookId = "282420687708111";
-		public const string facebookSecret = "ee52457996fd4d1584e435deaacdef7c";
-	}
-}
